8fec02239bce82de829b3ee0ddc813fae6ff0d30
Chau Hoai Thuong <40294239+MarkusKeithThomas@users.noreply.github.com>
2025-12-15 23:10:37 +0700

    resolve conflict (#6)

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React đã sửa ở kiem tra conflict</h1>
+      <h1>Vite + React đã sửa ở feat</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
